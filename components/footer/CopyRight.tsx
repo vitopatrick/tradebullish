ba14598ac58941e16c595a85667ab8fce330b161
theWarlock <patrickvito2012@gmail.com>
2023-09-24 13:12:14 +0100

    removed footer

--- components/footer/CopyRight.tsx
+++ components/footer/CopyRight.tsx
@@ -27,34 +27,6 @@ export default function CopyRight() {
         assessment of the person making the trading decisions and the terms of
         all the legal documentation.
       </div>
-      <div className="leading-loose text-neutral-500">
-        <span className="font-bold uppercase underline text-black">
-          Restricted countries:{" "}
-        </span>{" "}
-        NAGAMARKET Capital Ltd does not provide services for the residents of
-        certain countries, such as Afghanistan, Albania, American Samoa,
-        Anguilla, Australia, Austria, Barbados, Belarus, Belgium, Benin,
-        Bermuda, British Indian Ocean Territory, Bulgaria, Burkina Faso, Canada,
-        Cayman Islands, Central African Republic, China, Christmas Island, Cocos
-        (Keeling) Islands, Congo, Congo, The Democratic Republic of the,
-        Croatia, Cyprus, Czech Republic, Denmark, Estonia, Falkland Islands
-        (Malvinas), Finland, France, Germany, Gibraltar, Greece, Guam, Haiti,
-        Heard Island and McDonald Islands, Hungary, Iceland, Iran, Islamic
-        Republic of, Ireland, Isle of Man, Israel, Italy, Jamaica, Japan,
-        Jersey, Korea, Democratic People's Republic of, Latvia, Libyan Arab
-        Jamahiriya, Liechtenstein, Lithuania, Luxembourg, Mali, Malta,
-        Montserrat, Mozambique, Myanmar, Netherlands, New Zealand, Norfolk
-        Island, Norway, Palestinian Territory, Occupied, Pitcairn, Poland,
-        Portugal, Romania, Russian Federation, Saint Helena, Ascension and
-        Tristan Da Cunha, San Marino, Senegal, Serbia, Slovakia, Slovenia,
-        Somalia, South Georgia and the South Sandwich Islands, South Sudan,
-        Spain, Sri Lanka, Sweden, Switzerland, Syrian Arab Republic, Trinidad
-        and Tobago, Tunisia, Turks and Caicos Islands, Uganda, Ukraine, United
-        Kingdom and any other countries where the citizens have British proof of
-        identity (i.e. British Virgin Island, Gibraltar, Isle of Man etc.),
-        United States, U.S. Minor Islands, Vanuatu, Virgin Islands, British,
-        Virgin Islands, U.S., Yemen, and Zimbabwe.
-      </div>
     </div>
   );
 }
