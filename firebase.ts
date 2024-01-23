a90d13b2534cec2e8bcc7d7b4b08952b1793e68f
theWarlock <patrickvito2012@gmail.com>
2024-01-23 15:23:52 +0100

    changed the firebase and tawk.to

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDa6MEAelYuYqwgqjjmbbVj7Wzz709ThQA",
-  authDomain: "brokerbase-c03dd.firebaseapp.com",
-  projectId: "brokerbase-c03dd",
-  storageBucket: "brokerbase-c03dd.appspot.com",
-  messagingSenderId: "494274016587",
-  appId: "1:494274016587:web:dd21ceae3288ead55724ce",
+  apiKey: "AIzaSyDqLAVQ1LPnx02yi6e_UsJL4aj7L4-g1f4",
+  authDomain: "brokerbase-8f9a5.firebaseapp.com",
+  projectId: "brokerbase-8f9a5",
+  storageBucket: "brokerbase-8f9a5.appspot.com",
+  messagingSenderId: "804354409302",
+  appId: "1:804354409302:web:89cdd8f2eb75c6fc758cf7",
 };
 
 
--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -10,13 +10,12 @@ export const useChatSupport = () => {
       var s1: any = document.createElement("script"),
         s0: any = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/65466b97f2439e1631ebc51b/1hedh8j7p";
+      s1.src = "https://embed.tawk.to/65afcb6a8d261e1b5f56dd62/1hkrb56aq";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
     })();
   }, []);
 };
-
 
 
