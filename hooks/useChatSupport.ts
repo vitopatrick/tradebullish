6cebbe8a695fa87ad040af58ea04648f602b2261
warlock <fecypher@gmail.com>
2024-08-23 19:31:12 +0100

    f

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -6,7 +6,7 @@ export const useChatSupport = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.async = true;
-    script.src = "//code.tidio.co/id4kvojopc5yoefszmbcypn6enz85oth.js";
+    script.src = "";
 
     document.body.appendChild(script);
   }, []);
